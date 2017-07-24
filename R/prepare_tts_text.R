fd853ef4312c52cf6c0ce177147d29173f700986
Ian Moran <ian.moran11@gmail.com>
2017-07-24 18:24:46 +1000

    first commit

---
+++ R/prepare_tts_text.R
@@ -0,0 +1,24 @@
+
+#' Pepare tts text
+#'
+#' This function takes 
+#' @param text The text to be coverted.
+#' @param character_ratio The sensitivity with which lines with numbers and punctuation are excluded.
+#' @keywords tts
+#' @export
+#' @examples
+#' cat_function()
+#' 
+
+
+prepare_tts_text <-  function(text, character_ratio = .60){
+  text %>%   
+    stringr::str_split("(\r\n)|(\n)") %>% 
+    unlist %>% 
+    dplyr::data_frame(text = .) %>% 
+    dplyr::mutate(row_number = row_number()) %>% 
+    dplyr::mutate(text = stringr::str_replace_all(text, "[:space:]+"," ")) %>% 
+    dplyr::mutate(text = stringr::str_replace_all(text, "\\.+"," ")) %>% 
+    dplyr::mutate(az_ratio = stringr::str_count(text,"[A-Za-z]") / nchar(text) ) %>% 
+    dplyr::filter(az_ratio > character_ratio ) %>% .$text %>% paste(collapse = " ")}
+
