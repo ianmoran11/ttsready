9c1526d0a9d0fd4e2a052fd092845a776e7cf671
Ian <ian.moran11@gmail.com>
2017-10-29 15:48:33 +1100

    change code that removes full stops

--- R/prepare_tts_text.R
+++ R/prepare_tts_text.R
@@ -18,7 +18,7 @@ prepare_tts_text <-  function(text, character_ratio = .60){
     dplyr::data_frame(text = .) %>% 
     dplyr::mutate(row_number = row_number()) %>% 
     dplyr::mutate(text = stringr::str_replace_all(text, "[:space:]+"," ")) %>% 
-    dplyr::mutate(text = stringr::str_replace_all(text, "\\.+"," ")) %>% 
+ #  dplyr::mutate(text = stringr::str_replace_all(text, "\\.+"," ")) %>% 
     dplyr::mutate(az_ratio = stringr::str_count(text,"[A-Za-z]") / nchar(text) ) %>% 
     dplyr::filter(az_ratio > character_ratio ) %>% .$text %>% paste(collapse = " ")}
 
